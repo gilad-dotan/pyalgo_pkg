113193715f6777253821efb116046a559ea7392b
Vlad-clou <67387886+Vlad-clou@users.noreply.github.com>
2021-03-31 16:23:41 +0300

    Add files via upload

---
+++ pyalgo/basic_modules/sortalgo_default_values/sortalgo_default_values.py
@@ -0,0 +1,10 @@
+"""
+This function is contains the default
+variables for the 'sortalgo' module
+"""
+in_order_default_values = ['SmallToBig', 'BigToSmall']
+
+#class quick_sort_default_values:
+#    def __init__(self): pass
+#
+#    move_same_value_to_default_values = ['down', 'up']

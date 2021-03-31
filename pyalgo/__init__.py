c4f83d27f338e01220b26591a3cbb4d18386aec7
Vlad-clou <67387886+Vlad-clou@users.noreply.github.com>
2021-03-31 16:21:15 +0300

    Add files via upload

---
+++ pyalgo/__init__.py
@@ -0,0 +1,69 @@
+print("...DONE...")
+
+
+# python -m pip install --user --upgrade setuptools wheel
+# python -m pip install --user --upgrade twine
+
+# python setup.py sdist bdist_wheel
+
+# python -m twine upload --repository testpypi dist/*  <-- upload to test Pypi
+# python -m twine upload dist/*  <-- upload to Pypi
+
+
+
+
+#You can use
+#[Github-flavored Markdown](https://guides.github.com/features/mastering-markdown/)
+#to write your content with the markdown protocol.
+
+
+
+
+# // Mission Log \\
+#
+# algocryption.pipeline --> add "save_key" and "load_key"
+#
+# algocryption --> add to "simple columnar transposition" the option to have a 'chunk_size' as a list
+#
+#
+# algocryption --> add "MD5"
+# algocryption --> add "RC4"
+# algocryption --> add "RC5"
+# algocryption --> add "RC6"
+# algocryption --> add "DES"
+# algocryption --> add "IDEA"
+# algocryption --> add "HMAC"
+# algocryption --> add "Rijndael"
+# algocryption --> add "TripleDES"
+# algocryption --> add "fence gate"
+# algocryption --> add "Caesar cipher"
+# algocryption --> add "sha algorithms"
+# algocryption --> add "Vigenere Cipher"
+# algocryption --> add "Playfair Cipher"
+# algocryption --> add "monoalphabetic cipher"
+# algocryption --> add "MD5 encryption algorithm"
+# algocryption --> add "IDEA encryption algorithm"
+# algocryption --> add "HMAC encryption algorithm"
+# algocryption --> add "shamir secret sharing scheme"
+# algocryption --> add "Cryptographic hash functions"
+# algocryption --> add "Twofish encryption algorithm"
+# algocryption --> add "Blowfish encryption algorithm"
+# algocryption --> add "Advanced Encryption Standard (AES)"
+# algocryption --> add "ECC Asymmetric Encryption Algorithm"
+# algocryption --> add "Elliptic Curve Digital Signature Algorithm" (ECDSA)
+#
+# pytrainer --> ML_functions // add lambda to Normal_Equation
+# pytrainer --> ML_functions // complete Cost_function_Grad -> linear regression \\
+# pytrainer --> ML_functions // complete Cost_function -> logistic regression \\
+# pytrainer --> ML_functions // complete Cost_function_Grad -> logistic regression \\
+#
+# pytrainer --> ML_functions // complete gradient_descent \\
+# pytrainer --> ML_functions_Training // complete forward_propagation \\ (add forward propagation to neural network)
+#
+#
+# sortalgo --> try to optimize "quick sort"
+# sortalgo --> try to use recursion in "quick sort"
+#
+#
+# basic_modules --> in "default_functions" try to optimize "to_single_list"
+# basic_modules --> in "default_functions" try to use regression
